6c9bb57b0c4ff88b2173835cadc63803c8af60a2
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-08-30 18:52:51 -0800

    hw0 poem working. add window to read.

---
+++ HW0poem/HW0poem.cpp
@@ -0,0 +1,19 @@
+// HW0poem.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Lisa Jacklin
+//CS 201
+//8/28/2021
+//poem piece is a section of "The Raven" by Edgar Allan Poe
+
+#include <iostream>
+
+int main()
+{
+    std::cout << " Deep into that darkness peering, long I stood there wondering, fearing,\n";
+    std::cout << "Doubting, dreaming dreams no mortal ever dared to dream before \n";
+    std::cout << "But the silence was unbroken, and the stillness gave no token, \n";
+    std::cout << "And the only word there spoken was the whispered word, “Lenore ?\n";
+    std::cout << "This I whispered, and an echo murmured back the word, “Lenore!”—\n";
+    std::cout << "Merely thisand nothing more.\n";
+}
+
+
