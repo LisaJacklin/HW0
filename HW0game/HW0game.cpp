650c5d4bc5710def5edbc407a0d94b06a207bb01
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-08-30 22:53:52 -0800

    text input, \n for line change

---
+++ HW0game/HW0game.cpp
@@ -0,0 +1,17 @@
+// HW0game.cpp  
+// Lisa Jacklin
+// CS 201
+//8/30/2021
+
+
+#include <iostream>
+
+int main()
+{
+
+    //prints out video game quick description
+    std::cout << "Genshin Impact: an RPG that allows you to run around the \n  world with the main focus being finding the main character's brother.\n";
+
+
+}
+
